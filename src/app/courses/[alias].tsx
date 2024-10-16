de63263d6a8249234f6d6be7de54900eb7fd336e
mishok2004 <mishok2004@mail.ru>
2024-10-16 23:48:48 +0300

    getStaticPaths

--- src/app/courses/[alias].tsx
+++ src/app/courses/[alias].tsx
@@ -1,4 +1,4 @@
-import { GetStaticProps, GetStaticPropsContext } from 'next';
+import { GetStaticPaths, GetStaticProps, GetStaticPropsContext } from 'next';
 import axios from 'axios';
 import { withLayout } from '../../../layout/Layout';
 import { MenuItem } from '../../../interfaces/menu.interface';
@@ -6,11 +6,23 @@ import { TopPageModel } from '../../../interfaces/page.interface';
 import { ParsedUrlQuery } from 'querystring';
 import { ProductModel } from '../../../interfaces/product.interface';
 
+const firstCategory = 0;
 function Course({ menu, page, products }: CourseProps) {
-  return <></>;
+  return <>{products}</>;
 }
 
 export default withLayout(Course);
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const { data: menu } = await axios.post<MenuItem[]>(
+    process.env.NEXT_PUBLIC_DOMAIN + 'api/top-page/find',
+    { firstCategory },
+  );
+  return {
+    paths: menu.flatMap((m) => m.pages.map((p) => '/courses/' + p.alias)),
+    fallback: true,
+  };
+};
 
 export const getStaticProps: GetStaticProps<CourseProps> = async ({
   params,
